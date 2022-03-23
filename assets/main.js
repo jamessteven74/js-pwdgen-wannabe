0d5f7290d93257561602b1b4f785adea5a001198
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-23 15:02:18 +0100

    initial_commit_created_html_main_js

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+/*
+chiedi all'utente il suo nome.
+chiedi il suo cognome
+chiedi il suo colore preferito
+infine scrivi sulla pagina il risultato in questo formato: nomecognomecolorepreferito22
+*/
+
+alert('Ciao! fornisci nome cognome colore preferito')
