1fc5ae4d138b39a266214acc1529d5da808bced5
jamessteven74 <jamesstevenvictorien@gmail.com>
2022-03-23 16:48:13 +0100

    add name_surname_color_password

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,25 @@ chiedi il suo colore preferito
 infine scrivi sulla pagina il risultato in questo formato: nomecognomecolorepreferito22
 */
 
-alert('Ciao! fornisci nome cognome colore preferito')
+
+// //alert
+// alert('Ciao! fornisci nome cognome colore preferito');
+
+const full_name = prompt("scrivi il tuo nome")
+console.log(full_name)
+
+document.getElementById("first_name").innerHTML = 'Fabio'
+
+const surname = prompt("scrivi tuo cognome")
+console.log(surname)
+
+document.getElementById("surname").innerHTML = 'Pacifici'
+
+const color = prompt("quale è il tuo colore preferito")
+console.log(color)
+document.getElementById("fav_color").innerHTML = 'rosso'
+
+const password = prompt ("scrivi la tua password")
+console.log(password)
+
+document.getElementById("password").innerHTML = '80'
